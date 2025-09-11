53bddd3ae8194f55105b0664a68a52abf64019a3
71104 <a71104@gmail.com>
2025-09-11 13:50:36 +0200

    Add PostCSS.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
 export const App = () => (
   <>
-    <h1 className="font-sans text-3xl font-bold underline">💖 Hello World!</h1>
+    <h1 className="text-3xl font-bold underline">💖 Hello World!</h1>
     <p>Welcome to your Electron application.</p>
   </>
 );
--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -5,7 +5,20 @@ import { plugins } from "./webpack.plugins";
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+  use: [
+    {
+      loader: "style-loader",
+    },
+    {
+      loader: "css-loader",
+      options: {
+        importLoaders: 1,
+      },
+    },
+    {
+      loader: "postcss-loader",
+    },
+  ],
 });
 
 export const rendererConfig: Configuration = {
